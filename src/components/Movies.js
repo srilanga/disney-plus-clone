71dafd5b995e0839e608190505e949156fafd5f2
srilanga <2983rgt@gmail.com>
2022-05-07 01:32:47 +0900

    FIX: Detail page's double scroll after hovering

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -40,7 +40,7 @@ function Movies() {
 export default Movies
 
 const Container = styled.div`
-
+    padding: 0 0 10px;
 
 `
 
@@ -53,11 +53,14 @@ const Content = styled.div`
 const Wrap = styled.div`
     overflow: hidden;
     cursor: pointer;
-    border-radius: 10px;
-    border: 3px solid rgba(249, 249, 249, 0.1);
+    border-radius: 4px;
+    border: 4px solid rgba(249, 249, 249, 0.1);
     box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
         rgb(0 0 0 / 73%) 0px 16px 10px -10px;
-    transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+    transition: all 300ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+    //transition-duration: 300ms;
+    //transition-property: transform, box-shadow;
+    //transition-timing-function: ease-out;
 
     img {
         width: 100%;
